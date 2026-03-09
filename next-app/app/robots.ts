c79c8e32c1910d4717cb8310f3630abbef99f254
Andrei Zitti <zittiandrei@yahoo.com>
2026-03-09 14:30:10 +0100

    seo: allow AI crawlers in robots.txt

---
+++ next-app/app/robots.ts
@@ -0,0 +1,39 @@
+import { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/', '/_next/'],
+      },
+      {
+        userAgent: 'GPTBot',
+        allow: '/',
+      },
+      {
+        userAgent: 'ChatGPT-User',
+        allow: '/',
+      },
+      {
+        userAgent: 'PerplexityBot',
+        allow: '/',
+      },
+      {
+        userAgent: 'ClaudeBot',
+        allow: '/',
+      },
+      {
+        userAgent: 'Google-Extended',
+        allow: '/',
+      },
+      {
+        userAgent: 'anthropic-ai',
+        allow: '/',
+      },
+    ],
+    sitemap: 'https://www.sailingacademy.ro/sitemap.xml',
+    host: 'https://www.sailingacademy.ro',
+  };
+}
